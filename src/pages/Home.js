b031a0359f9a9194502bb14d6d674c9595728fb4
Jibi Abraham <jibiabraham@pikkol.com>
2019-07-15 15:06:35 +0530

    Added section 5

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,14 @@ import styles from "./Home.module.css";
 import { ReactComponent as LogoSvg } from "./logo.svg";
 import { ReactComponent as HeroSvg } from "./home_fold_1.svg";
 import { ReactComponent as HeroSvg2 } from "./home_fold_3.svg";
+import { ReactComponent as HeroSvg3 } from "./home_fold_5.svg";
 import DoneAll from "@material-ui/icons/DoneAll";
 import EuroSymbol from "@material-ui/icons/EuroSymbol";
 import AssignmentInd from "@material-ui/icons/AssignmentInd";
 import ArrowRightAlt from "@material-ui/icons/ArrowRightAlt";
+import Contacts from "@material-ui/icons/Contacts";
+import People from "@material-ui/icons/People";
+import AccountBalance from "@material-ui/icons/AccountBalance";
 
 const Logo = () => {
 	return <LogoSvg className={styles.logo} />;
@@ -136,17 +140,56 @@ const Home = () => {
 				</div>
 			</div>
 			<div className={styles.section_4}>
-				<h4>Live your dreams</h4>
+				<h4>Claim your destiny</h4>
 				<h2>It’s your time to shine.</h2>
 				<h2>We will help you do that.</h2>
 				<span>
-					Match your profile and qualifications to over 4000 Universities and
-					12000 careers. We will help you with guaranteed admissions.
+					Get your access to an exhaustive set of preparation recipes, experts
+					from top Universities, scholarships, and full admissions support
+					services
 				</span>
 				<HeroSvg2 className={styles.hero_svg} />
 				<div className={styles.find_out_more_container}>
 					Find out more <ArrowRightAlt />
 				</div>
+			</div>
+			<div className={styles.section_5}>
+				<h4>More than a platform</h4>
+				<h2>
+					Ensuring that the dream of an international career gets the right
+					start
+				</h2>
+				<div>
+					<div className={styles.transparent_icon_container}>
+						<Contacts />
+					</div>
+					<h3>Complete Preparation</h3>
+					<span>
+						Course & Recipes for Study goals, IELTS prep, Application essays &
+						more
+					</span>
+				</div>
+				<div>
+					<div className={styles.transparent_icon_container}>
+						<People />
+					</div>
+					<h3>Guaranteed Funding</h3>
+					<span>
+						Through EdCoins* received when you sign-up. Cover your Application
+						or other costs
+					</span>
+				</div>
+				<div>
+					<div className={styles.transparent_icon_container}>
+						<AccountBalance />
+					</div>
+					<h3>Guaranteed Admit</h3>
+					<span>
+						To the best-fit University programs matching your profile and study
+						goals
+					</span>
+				</div>
+				<HeroSvg3 className={styles.hero_svg} />
 			</div>
 		</div>
 	);
---
+++ storybook-static/precache-manifest.e7c56ad1480cf21e4829d33dace95703.js
@@ -0,0 +1,39 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635ceccd3492ccfff6cd4df143ba82fa",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "3d0f80f47fb5e03946d9",
+    "url": "main.css"
+  },
+  {
+    "url": "main.d9333a9fc2245624ffb7.bundle.js"
+  },
+  {
+    "url": "runtime~main.d9333a9fc2245624ffb7.bundle.js"
+  },
+  {
+    "revision": "4bf4a3785465e88b7da9a261254ae2b1",
+    "url": "static/media/home_bg.4bf4a378.svg"
+  },
+  {
+    "revision": "6a95164cc2b2be96882eb88822c3665d",
+    "url": "static/media/home_fold_1.6a95164c.svg"
+  },
+  {
+    "revision": "e1dbaee92f768e452396f6da63db3bed",
+    "url": "static/media/home_fold_3.e1dbaee9.svg"
+  },
+  {
+    "revision": "99a11b05cbb211a38bfcda94776d9da2",
+    "url": "static/media/home_fold_5.99a11b05.svg"
+  },
+  {
+    "revision": "5cd138863bcd2f6f8ebaed0965821d57",
+    "url": "static/media/logo.5cd13886.svg"
+  },
+  {
+    "url": "vendors~main.d9333a9fc2245624ffb7.bundle.js"
+  }
+]);
